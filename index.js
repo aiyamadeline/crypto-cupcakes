2f3e3b86da7d97c154a9a562eaf6a29d8e28d86d
aiyamadeline <aiyaedwards123@gmail.com>
2023-02-03 11:19:23 +0000

    app works

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
-// TODO - require express-openid-connect and destructure auth from it
-
+const { auth, requiresAuth } = require('express-openid-connect');
 const { User, Cupcake } = require('./db');
+const { getUser } = require('./middleware/getUser');
+// TODO - require express-openid-connect and destructure auth from it
+const { SECRET, BASE_URL, CLIENT_ID, ISSUER_BASE_URL } = process.env;
+
+const config = {
+  authRequired: true,
+  auth0Logout: true,
+  secret: SECRET,
+  baseURL: BASE_URL,
+  clientID: CLIENT_ID,
+  issuerBaseURL: ISSUER_BASE_URL,
+};
+
 
 // middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(auth(config));
+app.use(getUser);
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
@@ -20,6 +34,26 @@ app.use(express.urlencoded({extended:true}));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+
+
+app.get('/', (req, res, next) => {
+  try{
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  } catch(error){
+    console.log(error)
+    next(error)
+  }
+});
+
+app.get('/profile', requiresAuth(), (req,res,next) => {
+  try{
+    console.log(req.user);
+    res.send(req.user);
+  }catch (error) {
+    console.log(error);
+    next(error)
+  }
+})
 
 app.get('/cupcakes', async (req, res, next) => {
   try {
